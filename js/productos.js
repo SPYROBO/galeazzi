aa0ac40437e65781e9ac02e3062a0787b0706816
SPYROBO <alejandrolunaalvarez2007@gmail.com>
2024-11-06 12:13:01 -0300

    ticket aún no se ha terminado

--- js/productos.js
+++ js/productos.js
@@ -1,19 +1,43 @@
+str = ''
 function crearTicket(id, nombre, precio, desc){
-    str = ''
     cont = 1
-    str += "<tr>"
-    str += "<td><button id='a" + id + "' class='btn btn-success btn-sm'>+</button>" + " " + "<button id='e" + id + "' class='btn btn-danger btn-sm'>X</button> </td>"
-    str += "<td>"+ nombre +"</td>"
-    str += "<td>"+ cont +"</td>"
-    str += "<td>"+ precio +"</td>"                
-    str += "<td>"+ desc +"</td>"
-    if(desc == 0){
-        str += "<td>"+ precio +"</td>"  
-    }else{
-        str += "<td>"+Math.round(precio/desc+precio, -1)+"</td>"
-    }                
-    str += '</tr>'       
-    document.getElementById('ticket').innerHTML = str
+    if(!document.getElementById("a"+id)){
+        str += "<tr>"
+        str += "<td><button id='a" + id + "' class='btn btn-success btn-sm' onclick='sumarproducto(" + id + ")'>+</button>" + " " + "<button id='e" + id + "' class='btn btn-danger btn-sm' onclick='restar()'>X</button> </td>"
+        str += "<td>"+ nombre +"</td>"
+        str += "<td id='cant" + id + "'>"+ cont +"</td>"
+        str += "<td id='precio" + id + "'>"+ precio +"</td>"                
+        str += "<td id='descuento" + id + "'>"+ desc +"</td>"
+        if(desc == 0){
+            str += "<td id='final" + id + "'>"+ precio +"</td>"  
+        }else{
+            str += "<td id='final" + id + "'>"+Math.round(precio/desc+precio, -1)+"</td>"
+        }                
+        str += '</tr>'       
+        document.getElementById("ticket").innerHTML = str
+    } else {
+        alert("Añadalo desde el otro lado")
+    }
+
+}
+
+function sumarproducto(id){
+document.getElementById('form_ticket').addEventListener("submit", function(event){
+    event.preventDefault();
+})
+let productolocal = document.getElementById("cant" + id);
+contador = parseInt(productolocal.textContent);
+contador++
+productolocal.innerHTML = contador;
+descuento_total = 0
+if(parseInt(document.getElementById("descuento"+id).textContent) != 0){
+    descuento_total = parseInt(document.getElementById("descuento"+id).textContent) * 0.01
+}
+document.getElementById("final" + id).innerHTML = (contador * parseInt(document.getElementById("precio" + id).textContent) - descuento_total)
+}
+
+function restar(){
+
 }
 
 setTimeout(function() {
