2d919174d23ef0f4c777645a251f7db405de7dcf
SPYROBO <alejandrolunaalvarez2007@gmail.com>
2024-11-12 22:31:24 -0300

    php casi terminado

--- php/compra_efectuada.php
+++ php/compra_efectuada.php
@@ -3,9 +3,10 @@ session_start();
 require_once('env.php');
 if(isset($_SESSION['id_empleado'])){
 if(isset($_POST['ticket'])){
-    if(count('ticket') > 1){
+    $ticket = $_POST['ticket'];
+    var_dump($ticket);
+    if(count($ticket) > 1){
         $id_emp = intval($_SESSION['id_empleado']);
-        $ticket = $_POST['ticket'];
         $suma = 0;
         for($i = 0; $i < count($ticket)-1; $i++){
             $suma += $ticket[$i]['precio_final'];
@@ -14,10 +15,20 @@ if(isset($_POST['ticket'])){
         $sql = "INSERT INTO ventas (num_factura ,dni_cliente, id_empleado, fecha_venta, total, id_tipo_pago) VALUES (?,?,?,?,?,?) ";
         $result = $conn->prepare($sql);
         $result -> execute([1231,end($ticket)[0],$id_emp,date("Y-m-d"),$suma,end($ticket)[1]]);
+        $sql = "SELECT IDENT_CURRENT ('ventas')";
+        $stmt = $conn->prepare($sql);
+        $stmt -> execute();
+        $id_venta = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        var_dump($id_venta);
         for($i = 0; $i < count($ticket)-1; $i++){
             $sql = "INSERT INTO detalle_venta (id_venta, id_producto, cant_producto) VALUES (?,?,?)";
             $result = $conn->prepare($sql);
-            $result->execute()
+            $result->execute([intval($id_venta[0]),intval($ticket[$i]["id"]),intval($ticket[$i]["cantidad"])]);
+            $sql = "UPDATE stock SET cantidad = cantidad - :cant WHERE id_producto = :id";
+            $result = $conn->prepare($sql);
+            $result->bindParam(':cant',intval($id_venta[0]));
+            $result->bindParam(':id',intval($ticket[$i]["id"]));
+            $result->execute();
         }
     }
 }
